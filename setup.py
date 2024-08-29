21fd935dde97b7932ca7b768a2ddd9ae34d65201
Alrond <ForrestZengMusic@gmail.com>
2024-08-29 15:25:34 -0700

    requirements edit

--- setup.py
+++ setup.py
@@ -6,15 +6,12 @@ def read(fname):
 
 setup(
     name = "pywhitakers",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Forrest Zeng",
     author_email = "forrestzengmusic@gmail.com",
     description = ("An API package to latin-words.com that returns the most likely Latin translation of a word."),
     license = "Apache 2.0",
     keywords = "Latin translation whitakers",
     long_description=read('README.md'),
-    install_requires=[
-        "requests",
-        "unidecode"
-    ]
+    install_requires=read('requirements.txt').splitlines()
 )
