ffb0d6ea6fbf9ed8c7c295c0e6daefd19e6bb21a
Alrond <ForrestZengMusic@gmail.com>
2024-08-29 15:17:26 -0700

    add install requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ def read(fname):
 
 setup(
     name = "pywhitakers",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Forrest Zeng",
     author_email = "forrestzengmusic@gmail.com",
     description = ("An API package to latin-words.com that returns the most likely Latin translation of a word."),
     license = "Apache 2.0",
     keywords = "Latin translation whitakers",
     long_description=read('README.md'),
+    install_requires=[
+        "requests",
+        "unidecode"
+    ]
 )
