1101017552eb04331ef423091338ce07d1324a7c
Alrond <ForrestZengMusic@gmail.com>
2024-08-29 15:06:41 -0700

    Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "pywhitakers",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Forrest Zeng",
     author_email = "forrestzengmusic@gmail.com",
     description = ("An API package to latin-words.com that returns the most likely Latin translation of a word."),
